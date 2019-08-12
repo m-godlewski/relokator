165f57588549f68810e1e18f1e308102253c3ea1
Mateusz <mateusz@mysocialseller.com>
2019-08-12 19:15:31 +0200

    basic registration added

--- src/core/core/urls.py
+++ src/core/core/urls.py
@@ -27,6 +27,6 @@ from .views import (
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
