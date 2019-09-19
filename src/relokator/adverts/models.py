fcccfed464c05b8d980bd059f55f2d4b1a72a132
Mateusz <mgodlewski25@gmail.com>
2019-09-19 15:30:28 +0200

    hot fixes

--- src/relokator/adverts/models.py
+++ src/relokator/adverts/models.py
@@ -30,10 +30,10 @@ class AdvertManager(models.Manager):
 
 class Advert(models.Model):
 
+    slug = models.SlugField(null=True, blank=True)
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     content = models.TextField(null=True, blank=True)
-    slug = models.SlugField(null=True, blank=True)
     create_date = models.DateTimeField(default=timezone.now)
     city = models.CharField(max_length=100, blank=False)
     address = models.CharField(max_length=100, blank=False)
