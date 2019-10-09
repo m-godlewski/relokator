691df514030d798a4a705fb51a1f72bee8a696f8
Mateusz <mateusz@mysocialseller.com>
2019-10-09 23:53:51 +0200

    added base account model

--- src/relokator/accounts/models.py
+++ src/relokator/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import(
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+
+class Account(AbstractBaseUser):
+    name = models.CharField(max_length=100, blank=False, verbose_name="Imię")
+    surname = models.CharField(max_length=100, blank=True, verbose_name="Nazwisko")
+    email = models.EmailField(max_length=100, unique=True, verbose_name="E-mail")
+    accepted_terms = models.BooleanField(default=False, verbose_name="Akceptuję regulamin serwisu")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email','accepted_terms']
