2b556d1c69f7f68653ecb0a2b49e3bd2d36e20e0
Mateusz <mateusz@mysocialseller.com>
2019-10-26 12:04:25 +0200

    little graphical fixes

--- src/relokator/accounts/models.py
+++ src/relokator/accounts/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import(
     BaseUserManager, AbstractBaseUser
 )
 
-"""
-    W trakcie tworzenia
-"""
+
+
 class Account(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True, verbose_name="E-mail")
     name = models.CharField(max_length=100, blank=False, verbose_name="Imię")
     surname = models.CharField(max_length=100, blank=True, verbose_name="Nazwisko")
-    email = models.EmailField(max_length=100, unique=True, verbose_name="E-mail")
-    accepted_terms = models.BooleanField(default=False, verbose_name="Akceptuję regulamin serwisu")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email','accepted_terms']
+    REQUIRED_FIELDS = ['']
