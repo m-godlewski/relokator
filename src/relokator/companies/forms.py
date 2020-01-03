398895a2cfa0a5370d0abb5082f9bf3351e79b92
Mateusz <mgodlewski25@gmail.com>
2020-01-03 19:01:54 +0100

    minor fixes + stable core doc added

--- src/relokator/companies/forms.py
+++ src/relokator/companies/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 from .models import Company
+from django.forms.widgets import ClearableFileInput
 
 
 class CompanyModelForm(forms.ModelForm):
     """
         class of form used for create and upated company objects
     """
+
+    # clearing logo and tariff image in settings
+    class ClearableFileInput(ClearableFileInput):
+        initial_text = 'Obecne'
+        input_text = 'Nowe'
+        clear_checkbox_label = 'Wyczyść'
+
+    logo = forms.ImageField(label='Select Profile Image',required = False, widget=ClearableFileInput)
+    tariff = forms.ImageField(label='Select Profile Image',required = False, widget=ClearableFileInput)
 
     class Meta:
         model = Company
